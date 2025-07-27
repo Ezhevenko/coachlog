14326bf9be1b34e5fa68db9270dda528da6db914
Ezhevenko <schien@protonmail.com>
2025-07-27 21:28:03 +0300

    Add global styles setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './ui/**/*.{ts,tsx}',
+    './*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
