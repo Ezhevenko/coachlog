e0f8e3c6ecb846d5b3f01957f41446dd998a1492
Ezhevenko <schien@protonmail.com>
2025-07-27 18:06:32 +0300

    Add dependencies and API helper

---
+++ lib/api.ts
@@ -0,0 +1,12 @@
+import { useAuthToken } from '../pages/index'
+
+export function useApiFetch() {
+  const token = useAuthToken()
+  return async function apiFetch(input: RequestInfo | URL, init: RequestInit = {}) {
+    const headers = {
+      ...(init.headers || {}),
+      ...(token ? { Authorization: `Bearer ${token}` } : {})
+    }
+    return fetch(input, { ...init, headers })
+  }
+}
